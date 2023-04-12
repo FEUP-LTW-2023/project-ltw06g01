c4a48912b16590715a20302f70942c6c6b26244f
Afonso Vaz Osorio <afonso.vo@gmail.com>
2023-04-12 12:16:05 +0100

    add requires

--- actions/open_ticket.action.php
+++ actions/open_ticket.action.php
@@ -1,4 +1,6 @@
 <?php
+    require_once(__DIR__ . '../database/connection.php');
+    require_once(__DIR__ . '../database/tickets.php');
     $db = getDatabaseConnection();
     addTicket($db, $_POST['uid'], $_POST['title'], $_POST['fulltext']);
     exit();
