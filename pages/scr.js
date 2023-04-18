6820b9bcb4fe5dacb4b5bc74fccb5936c79b19cc
goncalobcosta <93865484+goncalobcosta@users.noreply.github.com>
2023-04-18 12:30:54 +0100

    Add files via upload

--- pages/scr.js
+++ pages/scr.js
@@ -34,38 +34,56 @@ var user = {
 window.onload = function() {
   showButtons(user.type);
 };
+/*
+//Para ao dar login enviar a box do login para cima:
+const loginForm = document.querySelector('.form-box-login');
+loginForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  loginForm.classList.add('hidden');
+  setTimeout(() => {
+    window.location.href = 'mainpage.html';
+  }, 500);
+});
+*/
 
-// Selecionar os elementos de login e inscrição
-var loginSection = document.getElementById("login");
-var signupSection = document.getElementById("signup");
-console.log(signupSection);
 
-// Esconder a seção de inscrição inicialmente
-signupSection.style.display = "none";
+//Caso não tenha conta e queira dar signup ou vice versa, então fzemos animações com as boxs
+// selecione os elementos DOM
+const loginBox = document.querySelector('#login-box');
+const signupBox = document.querySelector('#signup-box');
+const loginButton = document.querySelector('#login-button');
+const signupButton = document.querySelector('#Register-button');
 
-// Adicionar um evento de clique no botão "Ainda não tens conta?"
-var signupButton = signupSection.querySelector("#login-button");
-console.log(signupButton);
-signupButton.addEventListener("click", function() {
-  // Esconder a seção de login
-  loginSection.style.display = "none";
-  // Mostrar a seção de inscrição
-  signupSection.style.display = "flex";
+// adicione o manipulador de eventos de clique aos botões
+loginButton.addEventListener('click', () => {
+// verifica se a caixa de login já está visível
+if (loginBox.style.display === 'block') {
+return; // não faz nada se já estiver visível
+}
+else{// esconde a caixa de inscrição e mostra a caixa de login
+signupBox.style.animation = 'slideOutRight 1s ease-in-out';
+setTimeout(() => {
+signupBox.style.display = 'none';
+loginBox.style.display = 'block';
+loginBox.style.animation = 'slideInLeft 1s ease-in-out';
+}, 900)
+}
 });
 
-// Adicionar um evento de clique no botão "Já tens conta?"
-var loginButton = loginSection.querySelector("#signup-button");
-loginButton.addEventListener("click", function() {
-  // Esconder a seção de inscrição
-  signupSection.style.display = "none";
-  // Mostrar a seção de login
-  loginSection.style.display = "flex";
+signupButton.addEventListener('click', () => {
+// verifica se a caixa de inscrição já está visível
+if (signupBox.style.display === 'block') {
+return; // não faz nada se já estiver visível
+}
+// esconde a caixa de login e mostra a caixa de inscrição
+else{
+loginBox.style.animation = 'slideOutLeft 1s ease-in-out';
+setTimeout(() => {
+loginBox.style.display = 'none';
+signupBox.style.display = 'block';
+signupBox.style.animation = 'slideInRight 1s ease-in-out';
+}, 900)
+
+}
 });
 
-// Esconder a seção de inscrição quando a página é carregada
-window.onload = function() {
-  showButtons(user.type);
-  signupSection.style.display = "none";
-  loginSection.style.display = "flex";
-
-};
