62199196dda66bca5898a13a45af1ff5248e3b13
Afonso Vaz Osorio <afonso.vo@gmail.com>
2023-04-19 11:37:14 +0100

    use lastInsertId on signup

--- database/client.php
+++ database/client.php
@@ -24,11 +24,7 @@
         $stmt = $db->prepare('INSERT INTO CLIENT(username, passHash, email, permissionLevel) VALUES (?, ?, ?, 0)');
         $result = $stmt->execute(array($username, hash('sha256', $password,), $email));
 
-        $stmt = $db->prepare('SELECT uid FROM CLIENT WHERE username = ?');
-        $stmt->execute(array($username));
-        $uid = $stmt->fetch();
-
-        return array(!($result === 0), $uid);
+        return array(!($result === 0), $db->lastInsertId());
     }
 
     function updateUserPerms($db, $id, $newLevel) {
