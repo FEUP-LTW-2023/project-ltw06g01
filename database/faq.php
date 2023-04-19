f266ac0a17aa10c5a59645aa59e0f148dcc0c8fc
Afonso Vaz Osorio <afonso.vo@gmail.com>
2023-04-19 12:15:51 +0100

    backend for assigning FAQ to a ticket

--- database/faq.php
+++ database/faq.php
@@ -8,8 +8,13 @@
 
     function getFAQItemTags($db, $id, $maxTags) {
         $stmt = $db->prepare('SELECT tag FROM faqtag WHERE fid = ? limit ?');
-        $stmt->execute();
+        $stmt->execute(array($id, $maxTags));
 
         return $stmt->fecthAll();
+    }
+
+    function assignFAQItemToTicket($db, $faqID, $tID) {
+        $stmt = $db->prepare('UPDATE TICKET SET faqitem = ? WHERE id = ?');
+        $stmt->execute(array($faqID, $tID));
     }
 ?>
