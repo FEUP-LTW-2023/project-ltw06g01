eb94cfab822137933f5de5ed21e2c8cd71c9de8e
goncalobcosta <93865484+goncalobcosta@users.noreply.github.com>
2023-04-21 02:16:47 +0100

    1

--- pages/scr.js
+++ pages/scr.js
@@ -1,7 +1,7 @@
 
 // Exemplo de autenticação 
 var user = {
-  type: 'admin'
+  type: 'client'
 };
 //Caso não tenha conta e queira dar signup ou vice versa, então fzemos animações com as boxs
 // selecione os elementos DOM
@@ -12,7 +12,11 @@ const signupButton = document.querySelector('#Register-button');
 const loginForm = document.querySelector('.submit-login');
 const signupForm = document.querySelector('.submit-signup');
 const logoutForm = document.querySelector('.logout-box');
-const logoutBox = document.querySelector('.logout-box')
+const logoutBox = document.querySelector('.logout-box');
+const beforeMenuBox = document.querySelector('.before-menu');
+const menuBox = document.querySelector('.menu');
+const afterLoginBox = document.querySelector('.after-login');
+
 
 // adicione o manipulador de eventos de clique aos botões
 loginButton.addEventListener('click', () => {
@@ -47,15 +51,25 @@ signupBox.style.animation = 'slideInRight 1s ease-in-out';
 });
 
 //Para ao dar login enviar a box do login para cima e aparecer logout:
-loginForm.addEventListener('submit', (event) => {
+loginForm.addEventListener('click', (event) => {
   event.preventDefault();
   loginBox.style.animation = 'slideOutLoginToLogout 0.6s ease-in-out';
   logoutBox.style.display = "grid";
   logoutBox.style.animation = "slideInLogout 2s ease-in-out";
+  /* ainda a implementar
+  beforeMenuBox.style.display = "grid";
+  beforeMenuBox.style.animation = "slideInButtons 2s ease-in-out";
+  */
+
+  setTimeout(() => {
+    menuBox.style.display = "flex";
+    menuBox.style.animation = "slideInButtons 1.2s ease-in-out";
+  }, 500)
+  
   showButtons(user.type);
   setTimeout(() => {
-  loginBox.style.display = 'none';
-}, 500)
+    loginBox.style.display = 'none';
+  }, 500)
 });
 
 //Para ao dar signup enviar a box do signup para cima e aparecer logout:
@@ -64,6 +78,12 @@ signupForm.addEventListener('click', (event) => {
   signupBox.style.animation = 'slideOutLoginToLogout 0.6s ease-in-out';
   logoutBox.style.display = "grid";
   logoutBox.style.animation = "slideInLogout 2s ease-in-out";
+  beforeMenuBox.style.display = "grid";
+  beforeMenuBox.style.animation = "slideInButtons 2s ease-in-out";
+  setTimeout(() => {
+    menuBox.style.display = "flex";
+    menuBox.style.animation = "slideInButtons 2s ease-in-out";
+  }, 500)
   showButtons(user.type);
   setTimeout(() => {
   signupBox.style.display = 'none';
@@ -73,13 +93,17 @@ signupForm.addEventListener('click', (event) => {
 logoutForm.addEventListener('click', (event) => {
   event.preventDefault();
   logoutBox.style.animation = 'slideOutLoginToLogout 2s ease-in-out';
-  outButtons(user.type);
+  menuBox.style.animation = 'slideOutButtons 1s ease-in-out';
+
   setTimeout(() => {
-  loginBox.style.display = "grid";
-  loginBox.style.animation = "slideInLogout 2s ease-in-out";
-}, 1000)
+    outButtons(user.type);
+    loginBox.style.display = "grid";
+    loginBox.style.animation = "slideInLogout 1s ease-in-out";
+  }, 500)
+
   setTimeout(() => {
-  logoutBox.style.display = 'none';
+    logoutBox.style.display = 'none';
+    menuBox.style.display = 'none';
 }, 500)
 });
 
@@ -87,7 +111,7 @@ function showButtons(userType) {
   if(userType != 'none'){
   var clientButtons = document.querySelectorAll('.client-button');
     for (var i = 0; i < clientButtons.length; i++) {
-      clientButtons[i].style.animation = "slideInButtons 2s ease-in-out";
+      clientButtons[i].style.animation = "expand 2s ease-out";
       clientButtons[i].style.display = 'inline-grid';
     }
   
@@ -95,19 +119,19 @@ function showButtons(userType) {
   if (userType === 'admin') {
     var agentButtons = document.querySelectorAll('.agent-button');
     for (var i = 0; i < agentButtons.length; i++) {
-      agentButtons[i].style.animation = "slideInButtons 2s ease-in-out";
+      agentButtons[i].style.animation = "expand 2s ease-out";
       agentButtons[i].style.display = 'inline-grid';
     }
     var adminButtons = document.querySelectorAll('.admin-button');
     for (var i = 0; i < adminButtons.length; i++) {
-      adminButtons[i].style.animation = "slideInButtons 2s ease-in-out";
+      adminButtons[i].style.animation = "expand 2s ease-out";
       adminButtons[i].style.display = 'inline-grid';
     }
   } 
   else if (userType === 'agent') {
     var agentButtons = document.querySelectorAll('.agent-button');
     for (var i = 0; i < agentButtons.length; i++) {
-      clientButtons[i].style.animation = "slideInButtons 2s ease-in-out";
+      clientButtons[i].style.animation = "expand 2s ease-out";
       agentButtons[i].style.display = 'inline-grid';
     }
   } 
@@ -118,7 +142,7 @@ function outButtons(userType) {
   if(userType != 'none'){
   var clientButtons = document.querySelectorAll('.client-button');
     for (var i = 0; i < clientButtons.length; i++) {
-      clientButtons[i].style.animation = "slideOutButtons 2s ease-in-out";
+      clientButtons[i].style.animation = "slideOutButtons 1s ease-in-out";
       setTimeout(() => {
       clientButtons[i].style.display = 'none';
     }, 500)
@@ -128,14 +152,14 @@ function outButtons(userType) {
   if (userType === 'admin') {
     var agentButtons = document.querySelectorAll('.agent-button');
     for (var i = 0; i < agentButtons.length; i++) {
-      agentButtons[i].style.animation = "slideOutButtons 2s ease-in-out";
+      agentButtons[i].style.animation = "slideOutButtons 1s ease-in-out";
       setTimeout(() => {
       agentButtons[i].style.display = 'none';
     }, 500)
     }
     var adminButtons = document.querySelectorAll('.admin-button');
     for (var i = 0; i < adminButtons.length; i++) {
-      adminButtons[i].style.animation = "slideOutButtons 2s ease-in-out";
+      adminButtons[i].style.animation = "slideOutButtons 1s ease-in-out";
       setTimeout(() => {
       adminButtons[i].style.display = 'none';
     }, 500)
@@ -144,7 +168,7 @@ function outButtons(userType) {
   else if (userType === 'agent') {
     var agentButtons = document.querySelectorAll('.agent-button');
     for (var i = 0; i < agentButtons.length; i++) {
-      clientButtons[i].style.animation = "slideOutButtons 2s ease-in-out";
+      clientButtons[i].style.animation = "slideOutButtons 1s ease-in-out";
       setTimeout(() => {
       agentButtons[i].style.display = 'none';
     }, 500)
