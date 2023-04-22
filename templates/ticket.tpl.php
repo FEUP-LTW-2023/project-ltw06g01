632f7ee389a8a0b13919e6fe29b1109826dea876
Afonso Vaz Osorio <afonso.vo@gmail.com>
2023-04-22 14:08:21 +0100

    start work on editing ticket

--- templates/ticket.tpl.php
+++ templates/ticket.tpl.php
@@ -3,6 +3,13 @@ require_once(__DIR__ . '/../database/connection.php');
 require_once(__DIR__ . '/../database/departments.php');
 function drawTicketForm($ticket, $edit)
 {
+    if (isset($ticket)) {
+        $buttonText = "Editar";
+        $action = "../actions/edit_ticket.action.php";
+    } else {
+        $buttonText = "Enviar";
+        $action = "../actions/open_ticket.action.php";
+    }
 ?>
     <div class="container">
         <section id="ticket-form">
@@ -10,7 +17,7 @@ function drawTicketForm($ticket, $edit)
             <form>
                 <div>
                     <label for="department">Departamento:</label>
-                    <select id="department" name="department" <?php if (!$edit) echo 'disabled';?>>
+                    <select id="department" name="department" <?php if (!$edit) echo 'disabled'; ?>>
                         <?php if ($edit) {
                             foreach (getDepartments(getDatabaseConnection()) as $department) { ?>
                                 <option value=<?= $department['name'] ?>><?= $department['name'] ?></option>
@@ -23,13 +30,13 @@ function drawTicketForm($ticket, $edit)
                 </div>
                 <div>
                     <label for="title">Assunto:</label>
-                    <input type="text" id="subject" name="title" <?php if (!$edit) echo 'readonly';?> value=<?= $ticket['title'] ?>>
+                    <input type="text" id="subject" name="title" <?php if (!$edit) echo 'readonly'; ?> value=<?= $ticket['title'] ?>>
                 </div>
                 <div>
                     <label for="fulltext">Mensagem:</label>
-                    <textarea id="message" name="fulltext" <?php if (!$edit) echo 'readonly';?>><?= $ticket['text'] ?></textarea>
+                    <textarea id="message" name="fulltext" <?php if (!$edit) echo 'readonly'; ?>><?= $ticket['text'] ?></textarea>
                 </div>
-                <?php if ($edit) { ?> <button type="submit" formaction="../actions/open_ticket.action.php" formmethod="post">Enviar</button> <?php } ?>
+                <?php if ($edit) { ?> <button type="submit" formaction="../actions/open_ticket.action.php" formmethod="post"><?= $buttonText ?></button> <?php } ?>
             </form>
         </section>
     </div>
