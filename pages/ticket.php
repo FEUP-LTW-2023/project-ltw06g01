005cfa3384eb0f38e49aabfb968a546b56d96bc4
DanielCarneiro123 <danielmc2116@gmail.com>
2023-04-24 19:31:40 +0100

    titlos centrados + aside

--- pages/ticket.php
+++ pages/ticket.php
@@ -17,13 +17,18 @@
     $ticket = null;
   }
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
   <title>Enviar Ticket</title>
+  <script src = "nav.js" defer> </script>
+  <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
   <link rel="stylesheet" href="ticketstyle.css">
 </head>
 <body>
+
 <header class="header">
     <h1>Enviar Ticket</h1>
     <section id="logout" >
@@ -33,6 +38,43 @@
     </section>
 </header>
   <main>
+  <nav class="menu-lateral">
+  <div class="btn-expandir">
+    <ion-icon name="ticket-outline"></ion-icon>
+  </div>
+  <ul>
+    <li class="item-menu">
+      <a href = "#">
+        <span class = "icon"><ion-icon name="cog-outline"></ion-icon></span>
+        <span class = "txt-link">Tickets</span>
+      </a>
+    </li>
+    <li class="item-menu">
+      <a href = "#">
+        <span class = "icon"><ion-icon name="cog-outline"></ion-icon></span>
+        <span class = "txt-link">Tickets</span>
+      </a>
+    </li>
+    <li class="item-menu">
+      <a href = "#">
+        <span class = "icon"><ion-icon name="cog-outline"></ion-icon></span>
+        <span class = "txt-link">Tickets</span>
+      </a>
+    </li>
+    <li class="item-menu">
+      <a href = "#">
+        <span class = "icon"><ion-icon name="cog-outline"></ion-icon></span>
+        <span class = "txt-link">Tickets</span>
+      </a>
+    </li>
+    <li class="item-menu">
+      <a href = "#">
+        <span class = "icon"><ion-icon name="cog-outline"></ion-icon></span>
+        <span class = "txt-link">Tickets</span>
+      </a>
+    </li>
+  </ul>
+</nav>
     <?php drawTicketForm($ticket, true); ?>
   </main>
   <footer>
