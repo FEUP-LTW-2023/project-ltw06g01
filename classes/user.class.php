4cbfb7deac9b7dbbc4994fe20a264b3026ec5934
Afonso Vaz Osorio <afonso.vo@gmail.com>
2023-04-26 12:07:59 +0100

    create user class

---
+++ classes/user.class.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once(__DIR__ . '/../database/client.php');
+
+class Username
+{
+    public int $id;
+    public string $username;
+    public string $email;
+    public int $level;
+
+    public function __construct(int $id, string $username, string $email, int $level) {
+        $this->id = $id;
+        $this->username = $username;
+        $this->email = $email;
+        $this->level = $level;
+    }
+
+    static function getUsersAdmin(PDO $db): array {
+        $users = getAllUsers($db);
+
+        $res = array();
+        foreach ($users as $user) {
+            $res[] = new User($user['uid'], $user['username'], $user['email'], $user['permissionLevel']);
+        }
+
+        return $res;
+    }
+
+    static function getUser(PDO $db, int $uid): User {
+        $user = getUser($db, $uid);
+
+        return new User($user['uid'], $user['username'], $user['email'], $user['permissionLevel']);
+    }
+}
