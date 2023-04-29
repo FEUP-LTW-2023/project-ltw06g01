fc71938fd2527d4350b0afa907236e2657004c86
Afonso Vaz Osorio <afonso.vo@gmail.com>
2023-04-29 20:05:53 +0100

    fix api

--- api/api_users.php
+++ api/api_users.php
@@ -9,6 +9,7 @@ if (!isset($_SESSION['uid'])) {
 require_once(__DIR__ . '/../utils/validations.php');
 require_once(__DIR__ . '/../database/connection.php');
 require_once(__DIR__ . '/../database/client.php');
+require_once(__DIR__ . '/../classes/user.class.php');
 
 if (!hasAccessToPage(2, $_SESSION['level'])) {
     header('Location: /../pages/page.php');
