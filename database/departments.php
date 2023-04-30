4383d43ee0707459affc9236ccf4110fd3fbe5a6
Afonso Vaz Osorio <afonso.vo@gmail.com>
2023-04-30 21:15:10 +0100

    add more department functions

--- database/departments.php
+++ database/departments.php
@@ -5,4 +5,27 @@
 
         return $stmt->fetchAll();
     }
+
+    function getDepartmentsAgent($db, $aid) {
+        $stmt = $db->prepare('SELECT * FROM AGENTDEPARTMENT WHERE aid = ?');
+        $stmt->execute(array($aid));
+
+        return $stmt->fetchAll();
+    }
+
+    function toggleDepartment($db, $aid, $department) {
+        $stmt = $db->prepare('SELECT * FROM AGENTDEPARTMENT WHERE aid = ? AND department = ?');
+        $stmt->execute(array($aid, $department));
+
+        if (empty($stmt->fetch())) {
+            $stmt = $db->prepare('INSERT INTO AGENTDEPARTMENT VALUES (?,?)');
+            $stmt->execute(array($aid, $department));
+        }
+        else {
+            $stmt = $db->prepare('DELETE FROM AGENTDEPARTMENT WHERE aid = ? AND department = ?');
+            $stmt->execute(array($aid, $department));
+        }
+
+        return getDepartmentsAgent($db, $aid);
+    }
 ?>    
