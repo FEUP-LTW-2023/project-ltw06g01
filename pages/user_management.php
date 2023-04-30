92467f084226a92154bcae306d45a47825ad3c73
goncalobcosta <93865484+goncalobcosta@users.noreply.github.com>
2023-05-01 00:59:08 +0100

    some css

--- pages/user_management.php
+++ pages/user_management.php
@@ -21,25 +21,30 @@ $users = User::getUsersAdmin($db);
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-<title>All Tickets</title>
-  <link rel="stylesheet" href="geralstyle.css">
-  <script src="/../javascript/scr.js" defer></script>
-  <script src="/../javascript/user_promotion.js" defer></script>
-  <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-  <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
-</head>
-<body>
-  <header>
-      <?php   drawHeader(0, 4, "Manage users"); ?>
-  </header>
+  <head>
+  <title>All Tickets</title>
+    <link rel="stylesheet" href="geralstyle.css">
+    <script src="/../javascript/scr.js" defer></script>
+    <script src="/../javascript/user_promotion.js" defer></script>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+  </head>
+  <body>
+    <header>
+        <?php drawHeader(0, 4, "Manage users"); ?>
+    </header>
     <div id="nav">
       <?php drawSideBar(); ?>
     </div>
     <div id="content">
-    <?php
-      foreach ($users as $user) {
-        drawUserBox($db, $user, true);
-      }
-      ?>
+      <div id="all-users">
+        <?php
+          foreach ($users as $user) {
+            drawUserBox($db, $user, true);
+          }
+          ?>
+      </div>
     </div>
+  </body>
+</html>
+
--- templates/profile.tpl.php
+++ templates/profile.tpl.php
@@ -5,16 +5,48 @@ function drawUserBox(PDO $db, User $user, bool $admin)
 { ?>
     <div class="user-box">
         <input type="hidden" name="id" class="uid" value=<?= $user->id ?>>
-        <h3><?= $user->username ?></h3>
-        <p><?= $user->email ?></p>
+        <h3 id="name"><?= $user->username ?></h3>
+        <p id="email"><?= $user->email ?></p>
         <?php if ($user->level >= 1) {
             $departments = User::getAgentDepartments($db, $user->id); ?>
             <div class="user-departments">
                 <?php foreach ($departments as $department) { ?>
-                    <p><?= $department['department'] ?></p>
+                    <p id="departament"><?= $department['department'] ?></p>
                 <?php } ?>
             </div>
             <select name="departments" class="department-select"></select>
         <?php } ?>
-        <?php if ($admin) ?> <input name="n" type="number" class="user-promotion-button" value=<?= $user->level ?> min="0" max="2" step="1"> <?php ; ?>
-    <?php } ?>
+        <?php if ($admin) ?> <input name="n" type="number" class="user-promotion-button" value=<?= $user->level ?> min="0" max="2" step="1">
+    </div>
+<?php } ?>
+
+
+<?php 
+function drawProfile($curr_user){ 
+?>
+  <div class ="form-box-profile">
+      <div id="image">
+        <ion-icon name="person-circle-outline"></ion-icon>
+      </div>
+      <div id="name">
+        <ion-icon name="person-outline"></ion-icon>
+        <label> <?php echo $curr_user['username']; ?> </label>
+      </div>
+      <div id="email">
+          <ion-icon name="mail-outline"></ion-icon>
+          <label> <?php echo $curr_user['email']; ?></label>
+      </div>
+      <div id="type">
+          <ion-icon name="podium-outline"></ion-icon>
+          <?php if ($curr_user['permissionLevel'] >= 1): ?>
+            <label for="level">Type: Agent</label>
+          <?php elseif ($curr_user['permissionLevel'] == 2): ?>
+            <label for="level">Type: Admin</label>
+          <?php else: ?>
+            <label for="level">Type: Client</label>
+          <?php endif; ?>     
+       </div>
+  </div>
+<?php 
+}
+?>
