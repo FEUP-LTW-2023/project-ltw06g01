79a8ba17384db94728fad22801e6afa1cb586e3c
Afonso Vaz Osorio <afonso.vo@gmail.com>
2023-04-30 21:23:32 +0100

    add departments html

--- templates/profile.tpl.php
+++ templates/profile.tpl.php
@@ -7,8 +7,14 @@ function drawUserBox(PDO $db, User $user, bool $admin)
         <input type="hidden" name="id" class="uid" value=<?= $user->id ?>>
         <h3><?= $user->username ?></h3>
         <p><?= $user->email ?></p>
-        <?php if ($user->level >= 1) { ?>
-
-        <?php } ?>    
+        <?php if ($user->level >= 1) {
+            $departments = User::getAgentDepartments($db, $user->id); ?>
+            <div class="user-departments">
+                <?php foreach ($departments as $department) { ?>
+                    <p><?= $department ?></p>
+                <?php } ?>
+            </div>
+            <select name="departments" class="department-select"></select>
+        <?php } ?>
         <?php if ($admin) ?> <input name="n" type="number" class="user-promotion-button" value=<?= $user->level ?> min="0" max="2" step="1"> <?php ; ?>
     <?php } ?>
