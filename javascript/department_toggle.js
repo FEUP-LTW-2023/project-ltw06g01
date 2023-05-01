d0161db2c57d74bfefe7ae841b729cc4e60c3a9e
Afonso Vaz Osorio <afonso.vo@gmail.com>
2023-05-01 10:17:08 +0100

    continue script

--- javascript/department_toggle.js
+++ javascript/department_toggle.js
@@ -2,5 +2,15 @@ const userBoxes = document.querySelectorAll('.user-box')
 
 userBoxes.forEach((element) => {
     const userDepartments = element.querySelector('.user-departments')
+    if (userDepartments == null) return
     const departmentSelect = element.querySelector('.department-select')
+    const toggleButton = element.querySelector('.toggle-button')
+    const aid = element.querySelector('.uid')
+
+    toggleButton.addEventListener('click', async function() {
+        const department = departmentSelect.value 
+        const response = await fetch('../api/api_toggle_department.php?aid=' + aid.textContent + '&department=' + department)
+        const departments = response.json()
+        console.log(departments)
+    })
 })
--- templates/profile.tpl.php
+++ templates/profile.tpl.php
@@ -3,50 +3,52 @@ require_once(__DIR__ . '/../classes/user.class.php');
 
 function drawUserBox(PDO $db, User $user, bool $admin)
 { ?>
-    <div class="user-box">
-        <input type="hidden" name="id" class="uid" value=<?= $user->id ?>>
-        <h3 id="name"><?= $user->username ?></h3>
-        <p id="email"><?= $user->email ?></p>
-        <?php if ($user->level >= 1) {
-            $departments = User::getAgentDepartments($db, $user->id); ?>
-            <div class="user-departments">
-                <?php foreach ($departments as $department) { ?>
-                    <p id="departament"><?= $department['department'] ?></p>
-                <?php } ?>
-            </div>
-            <select name="departments" class="department-select"></select>
+  <div class="user-box">
+    <input type="hidden" name="id" class="uid" value=<?= $user->id ?>>
+    <h3 id="name"><?= $user->username ?></h3>
+    <p id="email"><?= $user->email ?></p>
+    <?php if ($user->level >= 1) {
+      $departments = User::getAgentDepartments($db, $user->id); ?>
+      <div class="user-departments">
+        <?php foreach ($departments as $department) { ?>
+          <p id="departament"><?= $department['department'] ?></p>
         <?php } ?>
-        <?php if ($admin) ?> <input name="n" type="number" class="user-promotion-button" value=<?= $user->level ?> min="0" max="2" step="1">
-    </div>
+      </div>
+      <?php if ($admin) { ?> <select name="departments" class="department-select"></select>
+        <button type="button" class="toggle-button">Toggle</button> <?php }
+                                                                } ?>
+    <?php if ($admin) ?> <input name="n" type="number" class="user-promotion-button" value=<?= $user->level ?> min="0" max="2" step="1">
+  </div>
 <?php } ?>
 
 
-<?php 
-function drawProfile($curr_user){ 
+<?php
+function drawProfile($curr_user)
+{
 ?>
-  <div class ="form-box-profile">
-      <div id="image">
-        <ion-icon name="person-circle-outline"></ion-icon>
-      </div>
-      <div id="name">
-        <ion-icon name="person-outline"></ion-icon>
-        <label> <?php echo $curr_user['username']; ?> </label>
-      </div>
-      <div id="email">
-          <ion-icon name="mail-outline"></ion-icon>
-          <label> <?php echo $curr_user['email']; ?></label>
-      </div>
-      <div id="type">
-          <ion-icon name="podium-outline"></ion-icon>
-          <?php if ($curr_user['permissionLevel'] >= 1): ?>
-            <label for="level">Type: Agent</label>
-          <?php elseif ($curr_user['permissionLevel'] == 2): ?>
-            <label for="level">Type: Admin</label>
-          <?php else: ?>
-            <label for="level">Type: Client</label>
-          <?php endif; ?>     
-       </div>
+  <div class="form-box-profile">
+    <div id="image">
+      <ion-icon name="person-circle-outline"></ion-icon>
+    </div>
+    <div id="name">
+      <ion-icon name="person-outline"></ion-icon>
+      <label> <?php echo $curr_user['username']; ?> </label>
+    </div>
+    <div id="email">
+      <ion-icon name="mail-outline"></ion-icon>
+      <label> <?php echo $curr_user['email']; ?></label>
+    </div>
+    <div id="type">
+      <ion-icon name="podium-outline"></ion-icon>
+      <?php if ($curr_user['permissionLevel'] >= 1) : ?>
+        <label for="level">Type: Agent</label>
+      <?php elseif ($curr_user['permissionLevel'] == 2) : ?>
+        <label for="level">Type: Admin</label>
+      <?php else : ?>
+        <label for="level">Type: Client</label>
+      <?php endif; ?>
+    </div>
   </div>
-<?php 
+<?php
 }
 ?>
