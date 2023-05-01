1c61e91fbdb6ecaec45af9ef3af25bafbd1da22d
Afonso Vaz Osorio <afonso.vo@gmail.com>
2023-05-01 01:07:35 +0100

    start making js script

---
+++ javascript/department_toggle.js
@@ -0,0 +1,6 @@
+const userBoxes = document.querySelectorAll('.user-box')
+
+userBoxes.forEach((element) => {
+    const userDepartments = element.querySelector('.user-departments')
+    const departmentSelect = element.querySelector('.department-select')
+})
