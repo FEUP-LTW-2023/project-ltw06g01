d9298cf75c89fb8fe4f48b3109842bb641c86fc4
Afonso Vaz Osorio <afonso.vo@gmail.com>
2023-05-03 10:59:12 +0100

    tag function

--- database/tags.php
+++ database/tags.php
@@ -13,3 +13,23 @@ function getTicketTags($db, $id) {
 
     return $stmt->fetchAll();
 }
+
+function setTicketTags($db, $id, $tags) {
+    $n = count($tags);
+    if ($n == 0) return;
+
+    $params = array();
+
+    $query = 'INSERT INTO TICKETTAG VALUES';
+    for (int $i = 0; $i < $n-1; $i++) {
+        $query = $query . '(?, ?),';
+        $params[] = $id;
+        $params[] = $tags[i];
+    }
+    $query = $query . '(?, ?)';
+    $params[] = $id;
+    $params[] = $tags[$n-1];
+
+    $stmt = $db->prepare($query);
+    $stmt->execute($params);
+}
