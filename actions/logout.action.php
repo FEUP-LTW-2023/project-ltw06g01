ddd88614172e014a71713ffa4670860b4fa49db8
Afonso Vaz Osorio <afonso.vo@gmail.com>
2023-05-10 10:58:40 +0100

    update remaining actions

--- actions/logout.action.php
+++ actions/logout.action.php
@@ -1,5 +1,12 @@
 <?php
-    session_start();
+    require_once(__DIR__ . '/../classes/session.class.php');
+
+    $session = new Session();
+    
+    if (!$session->isLoggedIn() || !$session->isValidSession($_POST['csrf'])) {
+        header('Location: page.php');
+    }
+
     require_once(__DIR__ . '/../database/connection.php');
     require_once(__DIR__ . '/../database/client.php');
 
