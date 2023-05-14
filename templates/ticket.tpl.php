86791c61b7e50d6e8c78bdf87bcdfd83280a9cab
Afonso Vaz Osorio <afonso.vo@gmail.com>
2023-05-14 11:08:05 +0100

    agent select displays current agent

--- templates/ticket.tpl.php
+++ templates/ticket.tpl.php
@@ -133,7 +133,7 @@ function drawAssignAgent($db, $ticket) {
         <input type="hidden" class="assign-id" value=<?= $ticket->id ?>>
         <select name="agents" class="agent-list" onchange="this.form.submit()"> <?php 
         foreach($agents as $agent) {
-            ?> <option value=<?= $agent->id ?>><?= $agent->username ?></option> <?php
+            ?> <option value=<?= $agent->id ?>><?= $agent->username ?> <?php if ($ticket->aid == $agent->id) echo 'selected'; ?></option> <?php
         } ?>
         </select>
     </form> <?php     
