55f11181ffabef34a0041d6af8c73650a32f2c88
DanielCarneiro123 <danielmc2116@gmail.com>
2023-05-15 18:33:07 +0100

    error

--- pages/users_tickets.php
+++ pages/users_tickets.php
@@ -10,6 +10,7 @@ if (!$session->isLoggedIn()) {
   require_once(__DIR__ . '/../classes/user.class.php');
   require_once(__DIR__ . '/../database/connection.php');
   require_once(__DIR__ . '/../database/status.php');
+  require_once(__DIR__ . '/../database/client.php'); 
   require_once(__DIR__ . '/../database/departments.php');
   require_once(__DIR__ . '/../templates/ticket.tpl.php');
   require_once(__DIR__ . '/../templates/common.tpl.php');
@@ -24,10 +25,9 @@ if (!$session->isLoggedIn()) {
   if ($_GET['ticket-filter-agent'] == 'default') $_GET['ticket-filter-agent'] = -1;
   $_GET['ticket-filter-agent'] = $_GET['ticket-filter-agent'] ?? -1;
 
-  $users = User::getUser($db);
+  $user_id = $_SESSION['uid'];
+  $users = getUser($db, $user_id);
   $departments = getDepartments($db);
-  
-
 
   if (empty($tickets)) {
     $tickets = null;}
@@ -49,26 +49,20 @@ if (!$session->isLoggedIn()) {
 </head>
 <body>
   <header>
-    <?php drawHeader(0, 4, "All Tickets"); ?>
+    <?php drawHeader(0, 4, "Your Tickets"); ?>
   </header>
   <div id="nav">
     <?php drawSideBar(); ?>
   </div>
   <div id="content">
     <form id="filtro" method="get" class="ticket-filter-container">
-      <label for="ticket-filter-status">Mostrar:</label>
-      <select name="ticket-filter-status" id="ticket-filter" onchange="this.form.submit()">
+      <label id="ticket-filter-status-title" for="ticket-filter-status">Status</label>
+      <select id="ticket-filter-status" class="ticket-filter" onchange="this.form.submit()">
         <option value="all" <?php if ($_GET['ticket-filter-status'] == 'all') echo 'selected'; ?>>Todos</option>
         <option value="open" <?php if ($_GET['ticket-filter-status'] == 'open') echo 'selected'; ?>>Abertos</option>
         <option value="closed" <?php if ($_GET['ticket-filter-status'] == 'closed') echo 'selected'; ?>>Fechados</option>
       </select>
-      <select name="ticket-filter-agent" class="ticket-filter" onchange="this.form.submit()">
-         <option value="default" <?php if ($_GET['ticket-filter-agent'] == 1) echo 'selected'; ?>></option>
-      <?php foreach ($users as $user) {
-        if ($user->level < 1 ) continue; ?>
-         <option value=<?= $user->id ?> <?php if ($_GET['ticket-filter-agent'] == $user->id) echo 'selected'; ?>><?= $user->username ?></option>
-      <?php } ?>
-  </select>
+      
   <select name="ticket-filter-department" class="ticket-filter" onchange="this.form.submit()">
         <option value="unassigned" <?php if ($_GET['ticket-filter-department'] == 'unassigned') echo 'selected'; ?>>Unassigned</option>
         <?php foreach ($departments as $department) { ?>
@@ -79,30 +73,27 @@ if (!$session->isLoggedIn()) {
     <?php
       $status = isset($_GET['ticket-filter-status']) ? $_GET['ticket-filter-status'] : 'open'; //// esta linha supostamente tem de sair?
       $tickets = Ticket::getFilteredTickets($db, $_GET['ticket-filter-status']);
-      $ticketsAgent = Ticket::getTicketsFromAgent($db, $_GET['ticket-filter-agent']);
-      $departmentTickts = Ticket::getTicketsFromDepartment($db, $_GET['ticket-filter-department']);
-      if ($_GET['ticket-filter-agent'] == 'default' || $_GET['ticket-filter-agent'] == $users) {
-        $finalTickets = Ticket::joinFilters($tickets, $ticketsAgent, $departmentTickets);
-    } else {
-        $finalTickets = [];
-    }
-     
-      echo var_dump($departmentTickets); ?>
-
+      $ticketsUser = Ticket::getTicketsFromUser($db, $users['uid']);
+      $departmentTickets = Ticket::getTicketsFromDepartment($db, $_GET['ticket-filter-department']);
+      $finalTickets = Ticket::joinFilters($tickets, $ticketsUser, $departmentTickets); ?>
+    
       <div id="allTickets">
-      
-        <?php foreach ($finalTickets as $ticket) {
+        <?php foreach ($finalTickets[0] as $ticket) {
                 $tags = getTicketTags($db, $ticket->id); ?>
                 <div id="ticket-display">
                 
                   <?php drawTicketForm($ticket, false, $tags); ?>
-
+                
                   <div id="options">
-                      <div id="filters">
-                        <?php drawAssignAgent($db, $ticket);
-                              drawChangeStatus($db, $ticket);
-                              drawPriorityButtons($ticket); ?>
-                      </div>
+                        <div id="filters-toggle">
+                            <ion-icon id="settings-not-hover" name="settings-outline"></ion-icon>
+                            <ion-icon id="settings-hover" name="settings"></ion-icon>
+                        </div>
+                        <div id="filters-container">
+                            <?php drawAssignAgent($db, $ticket);
+                                  drawChangeStatus($db, $ticket); 
+                                  drawPriorityButtons($ticket); ?>
+                        </div>
 
                       <a href="/../pages/view_ticket.php?id=<?php echo $ticket->id ?>">
                         <ion-icon id="view-not-hover" name="eye-outline"></ion-icon>
