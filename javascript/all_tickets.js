6bf857e240cb9f8a660a01fb69636a779d5f33c2
goncalobcosta <93865484+goncalobcosta@users.noreply.github.com>
2023-05-18 18:43:21 +0100

    .

--- javascript/all_tickets.js
+++ javascript/all_tickets.js
@@ -24,6 +24,30 @@ filtersButton.addEventListener('click', function() {
     filtersContainer.style.display = 'none';
     filtersButton.style.borderRadius = '15px';
   }
+
+  ajustarLarguraElementos();
+
 });
+
+
+function ajustarLarguraItensMenu() {
+  var itemMenus = document.querySelectorAll('.item-menu');
+  var maxWidth = 0;
+
+  // Encontra a largura máxima
+  for (var i = 0; i < filtersContainer.length; i++) {
+    var width = itemMenus[i].offsetWidth;
+    if (width > maxWidth) {
+      maxWidth = width;
+    }
+  }
+
+  // Define a largura máxima para todos os itens de menu
+  for (var i = 0; i < itemMenus.length; i++) {
+    itemMenus[i].style.width = maxWidth + 'px';
+  }
+}
+
+
 
 
