99ea7bef62f3b6b3721e93b8541014ef65489a11
Afonso Vaz Osorio <afonso.vo@gmail.com>
2023-05-20 14:09:47 +0100

    add csrf inputs

--- templates/admin.tpl.php
+++ templates/admin.tpl.php
@@ -1,32 +1,40 @@
 <?php
 
-function drawRemoveDepartment($department) { ?>
+function drawRemoveDepartment($department)
+{ ?>
     <li>
         <form>
-          <input type="hidden" name="department" value=<?= $department ?>>
-          <?= $department ?> <button type="submit" formmethod="post" formaction="../actions/delete_department.action.php">Delete</button>
+            <input type="hidden" name="csrf" value=<?= $_SESSION['csrf'] ?>>
+            <input type="hidden" name="department" value=<?= $department ?>>
+            <?= $department ?> <button type="submit" formmethod="post" formaction="../actions/delete_department.action.php">Delete</button>
         </form>
     </li>
 <?php }
 
-function drawRemoveStatus($status) { ?>
+function drawRemoveStatus($status)
+{ ?>
     <li>
         <form>
-          <input type="hidden" name="status" value=<?= $status ?>>
-          <?= $status ?> <button type="submit" formmethod="post" formaction="../actions/delete_status.action.php">Delete</button>
+            <input type="hidden" name="csrf" value=<?= $_SESSION['csrf'] ?>>
+            <input type="hidden" name="status" value=<?= $status ?>>
+            <?= $status ?> <button type="submit" formmethod="post" formaction="../actions/delete_status.action.php">Delete</button>
         </form>
     </li>
 <?php }
 
-function drawAddDepartment() { ?>
+function drawAddDepartment()
+{ ?>
     <form>
+        <input type="hidden" name="csrf" value=<?= $_SESSION['csrf'] ?>>
         <input type="text" name="department">
         <button type="submit" formmethod="post" formaction="../actions/add_department.action.php">Add</button>
     </form>
 <?php }
 
-function drawAddStatus() { ?>
+function drawAddStatus()
+{ ?>
     <form>
+        <input type="hidden" name="csrf" value=<?= $_SESSION['csrf'] ?>>
         <input type="text" name="status">
         <button type="submit" formmethod="post" formaction="../actions/add_status.action.php">Add</button>
     </form>
